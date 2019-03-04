df98827337136a76473faae24b011ec6320628cb
Serob Balyan <serob.balyan@gmail.com>
2019-03-05 02:13:26 +0300

    Remove StringTest class.

--- H3/src/main/java/StringTest.java
+++
@@ -1,8 +0,0 @@
-public class StringTest {
-    public static void main(String[] args) {
-        String s = "asdf";
-        for(String el: s.split("\\.")){
-            System.out.println("EL: " + el);
-        }
-    }
-}
